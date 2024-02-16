de5f223e476bc8074429dc85bf8470b4ec9d577e
Shane Page <shane.page@efleets.com>
2024-02-16 14:58:46 -0600

    better exception logic

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 import os
 import psutil
 
-BROWSERS = ["chromium", "firefox" , "vivaldi", "brave", "opera", "msedge", "chrome","postman"]
+BROWSERS = ["chromium", "firefox" , "vivaldi", "brave",
+            "opera", "msedge", "chrome","postman"]
 
 def chill_browsers(kill=False):
 
@@ -10,37 +11,36 @@ def chill_browsers(kill=False):
 
     process_ids = psutil.pids()
     for process_id in process_ids:
-        process = psutil.Process(process_id)
-        # print(f"Process ID: {process_id}, Name: {process.name()}")
 
-        for browser in BROWSERS:
-            if browser in process.name():
+        try:
+            process = psutil.Process(process_id)
+            # print(f"Process ID: {process_id}, Name: {process.name()}")
 
-
-                browser_pids.append(process_id)
-
-                if kill:
-                    try:
+            for browser in BROWSERS:
+                if browser in process.name():
+                    if kill:
                         process.kill()
-                    except Exception as e:
-                        print(f"Error killing process: {e}")
-                    continue
+                    else:
+                        print(f"Browser: {browser}, PID: {process_id}")
+                        print(f"  Nice before: {process.nice()}")
+                        print(f"  Cpu% : {process.cpu_percent(interval=1)}")
 
-                print(f"Browser: {browser}, PID: {process_id}")
-                print(f"Nice value: {process.nice()}")
-                print(f"cpu% : {process.cpu_percent(interval=1)}")
+                        # affinity
+                        process.cpu_affinity([0, 1])
+                        # priority
+                        if psutil.WINDOWS:  # Windows (either 32-bit or 64-bit)
+                            process.nice(psutil.IDLE_PRIORITY_CLASS)
+                        elif psutil.LINUX:  # linux
+                            process.nice(psutil.IOPRIO_CLASS_IDLE)
+                        else:  # MAC OS X or other
+                            process.nice(20)
+                        print(f"  Nice after: {process.nice()}")
 
-                # affinity
-                process.cpu_affinity([0, 1])
-                # priority
-                if psutil.WINDOWS:  # Windows (either 32-bit or 64-bit)
-                    process.nice(psutil.IDLE_PRIORITY_CLASS)
-                elif psutil.LINUX:  # linux
-                    process.nice(psutil.IOPRIO_CLASS_IDLE)
-                else:  # MAC OS X or other
-                    process.nice(20)
-                print(f"Nice value: {process.nice()}")
+                    # it worked
+                    browser_pids.append(process_id)
 
+        except Exception as e:
+            print(f"Error w process: {e}")
 
 
     print('chilled', browser_pids)
